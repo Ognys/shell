fd5588c0fc6b02935fb8cbdd811a1c89c104a37f
Yaroslav Zakharov <ogny9@mail.ru>
2026-07-27 10:40:16 +0300

    Implement cd command

--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+#include <unistd.h>
+#include <filesystem>
+
+void execute_command(std::vector<std::string> args);
 
 int main() {
 
     std::string str;
+    std::vector<std::string> str_split;
+
+    const char* home = std::getenv("HOME");
+
+    chdir(home);
 
     while(true) {
-        std::cout << "shell> ";
+        std::cout << "shell:" << std::filesystem::current_path() << "> ";
         getline(std::cin, str);
-        std::cout << str << std::endl;
+        boost::split(str_split, str, boost::is_any_of(" "));
+        execute_command(str_split);
+    }
+}
+
+void execute_command(std::vector<std::string> args) {
+    if(args.empty())
+        return;
+    else if(args[0] == "cd")
+    {
+        chdir(args[1].c_str());
     }
 }
