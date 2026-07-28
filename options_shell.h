b90d4961208d67360c7cc441efc8e968f7979259
Yaroslav Zakharov <ogny9@mail.ru>
2026-07-28 16:26:40 +0300

    Перечисление состояний кавычек перенесено в приватную часть класса options_shell

--- options_shell.h
+++ options_shell.h
@@ -5,18 +5,18 @@
 #include<string>
 #include <unordered_map>
 
-    enum class quotes {
-        NONE,
-        DOUBLE_QUOTES,
-        SINGLE_QUOTES
-    };
+
 
 class options_shell {
-
-
 
 public:
     static std::vector<std::string> shell_pars(std::string str);
     static std::unordered_map<std::string, std::string> variables;
 
+private:
+    enum class quotes {
+        NONE,
+        DOUBLE_QUOTES,
+        SINGLE_QUOTES
+    };
 };
